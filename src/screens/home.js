834e4a63f432f1bf672f8d8216d87f6250f7ff88
Alik-da-Geek <aliksemelianov@gmail.com>
2022-04-22 21:08:51 -0400

    initial commit

---
+++ src/screens/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/main.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="centeredText">
+        <ol>
+          <li>Test</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Home;
