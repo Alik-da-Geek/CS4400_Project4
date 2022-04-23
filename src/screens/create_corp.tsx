c2ff6c608ef642d224732f6fd91c068ce11843ac
Alik-da-Geek <aliksemelianov@gmail.com>
2022-04-23 18:01:33 -0400

    finished Q1 frontend

--- src/screens/create_corp.tsx
+++ src/screens/create_corp.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/main.css";
+import "../styles/forms.css";
 import { CreateCorpState } from '../types/Props'
 
 class CreateCorp extends React.Component<{}, CreateCorpState> {
@@ -13,15 +15,42 @@ class CreateCorp extends React.Component<{}, CreateCorpState> {
     };
 
     this.handleCorpIDChange = this.handleCorpIDChange.bind(this);
+    this.handleLongNameChange = this.handleLongNameChange.bind(this);
+    this.handleShortNameChange = this.handleShortNameChange.bind(this);
+    this.handleReservedAssetsChange = this.handleReservedAssetsChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearState = this.clearState.bind(this);
   }
 
   handleCorpIDChange(event) {
     this.setState({ corpID: event.target.value });
   }
+  handleLongNameChange(event) {
+    this.setState({ longName: event.target.value });
+  }
+  handleShortNameChange(event) {
+    this.setState({ shortName: event.target.value });
+  }
+  handleReservedAssetsChange(event) {
+    this.setState({ reservedAssets: event.target.value });
+  }
+
+  clearState(event) {
+    console.log('cleared')
+    this.setState({
+      corpID: '',
+      longName: '',
+      shortName: '',
+      reservedAssets: 0
+    })
+    event.preventDefault();
+
+  }
 
   handleSubmit(event) {
-    alert('A corporation was created\n');
+    console.log("corporation created")
+    console.log(this.state.corpID + ", " + this.state.longName + ", " + this.state.shortName + ", " + this.state.reservedAssets)
+    this.clearState(event)
     event.preventDefault();
   }
 
@@ -29,15 +58,41 @@ class CreateCorp extends React.Component<{}, CreateCorpState> {
     return (
       <div className="container">
         <div className="mainHeader">
-          <h1 className="mainTitle">Q1: Create Corporation</h1>
+          <h6><Link to="../">Home</Link></h6>
+          <h1>Q1: Create Corporation</h1>
         </div>
-        <div className="createCorpForm">
+        <div className="formContainer">
           <form onSubmit={this.handleSubmit}>
-            <label>
-              Corporation ID:
+            <div className="formItem">
+              <label>
+                Corporation ID:
+              </label>
               <input type="text" value={this.state.corpID} onChange={this.handleCorpIDChange} />
-            </label>
-            <input type="submit" value="Submit" />
+            </div>
+            <div className="formItem">
+              <label>
+                Long Name:
+              </label>
+              <input type="text" value={this.state.longName} onChange={this.handleLongNameChange} />
+            </div>
+            <div className="formItem">
+              <label>
+                Short Name:
+              </label>
+              <input type="text" value={this.state.shortName} onChange={this.handleShortNameChange} />
+            </div>
+            <div className="formItem">
+              <label>
+                Reserved Assets:
+              </label>
+              <input type="text" value={this.state.reservedAssets} onChange={this.handleReservedAssetsChange} />
+            </div>
+            <button onClick={this.clearState} className="formCancel">
+              Cancel
+            </button>
+            <button onClick={this.handleSubmit} className="formSubmit">
+              Submit
+            </button>
           </form>
         </div>
       </div>
